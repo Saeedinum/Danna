660487381308c6c9415da4c9c744d245a870d3ff
Saeedinum <mo7amed0ptr@gmail.com>
2024-06-28 10:30:00 +0300

    lottieFiles

--- src/Components/Notfound/Notfound.jsx
+++ src/Components/Notfound/Notfound.jsx
@@ -1,10 +1,26 @@
-import React from "react";
-import errorImg from "../../images/image 70.png";
+// import errorImg from "../../images/image 70.png";
+import {useRouteError, isRouteErrorResponse, Link} from "react-router-dom";
+import Lottie from "lottie-react";
+import errorAnimation from "./lotti.json";
 
 export default function Notfound() {
+	const error = useRouteError();
+	const [errorStatus, errorText] = isRouteErrorResponse(error) ? [error.status, error.statusText] : [404, "Page not found"];
 	return (
-		<div className='d-flex align-items-center justify-content-center'>
-			<img src={errorImg} className='w-50 mt-5' alt='Not found' />
-		</div>
+		<>
+			<div>
+				<h1 style={{textAlign: "center"}}>
+					Error: {errorStatus} - {errorText}
+				</h1>
+			</div>
+			<div style={{display: "flex", justifyContent: "center", fontSize: "1.5rem", alignItems: "center"}}>
+				<Link to='/' replace={true}>
+					<h2>Home</h2>
+				</Link>
+			</div>
+			<div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+				<Lottie animationData={errorAnimation} style={{width: "30%", height: "30%"}} />
+			</div>
+		</>
 	);
 }
