7ad09bf156f3827c3cb69d5fc795117227561a22
Saeedinum <mo7amed0ptr@gmail.com>
2024-06-30 15:07:50 +0300

    fetch articles , likes , comments

--- src/Components/Articles/Articles.jsx
+++ src/Components/Articles/Articles.jsx
@@ -1,4 +1,15 @@
-import{Component, useState} from "react";
+import {useState, useEffect, useRef} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import axios from "axios";
+import {baseURL} from "../../utils/baseURL";
+import {format, isToday, isYesterday, parseISO} from "date-fns";
+
+import Rectangle1 from "../../images/mother.jpg";
+import child from "../../images/card.jpg";
+import Ellipse from "../../images/child.png";
+import Rectangle4 from "../../images/mot.jpg";
+import comme from "../../images/Ellipse 36.png";
+
 import {
 	FacebookShareButton,
 	FacebookIcon,
@@ -13,31 +24,68 @@ import {
 	EmailShareButton,
 	EmailIcon,
 } from "react-share";
-import Rectangle1 from "../../images/mother.jpg";
-import child from "../../images/card.jpg";
-import Ellipse from "../../images/child.png";
-import Rectangle4 from "../../images/mot.jpg";
-import baby from "../../images/baby.png";
-import comme from "../../images/Ellipse 36.png";
-import {Link} from "react-router-dom";
 
-export default function Articles() {
-	const [show, setShow] = useState(false);
-	const [like, setLike] = useState(21),
-		[isLike, setIsLike] = useState(false);
+const Articles = () => {
+	const [articles, setArticles] = useState({
+		articles: [],
+		error: "",
+		loading: "",
+	});
+	const [popularArticles, setPopularArticles] = useState({
+		popularArticles: [],
+		error: "",
+		loading: "",
+	});
 
-	const onClickHandler = () => {
-		setComments((comments) => [...comments, comment]);
+	const fetchArticles = async () => {
+		try {
+			const response = await axios.get(baseURL + "articles", {
+				// params: {
+				// 	"page": 1,
+				// 	"sort": "-title",
+				// 	"likes[gt]": 1000,
+				// },
+			});
+			setArticles({
+				articles: response.data.result,
+				loading: false,
+				error: null,
+			});
+		} catch (err) {
+			setArticles({
+				articles: [],
+				loading: false,
+				error: err,
+			});
+		}
 	};
-	const onChangeHandler = (e) => {
-		setComment(e.target.value);
+
+	const fetchPopularArticles = async () => {
+		try {
+			const response = await axios.get(baseURL + "articles", {
+				params: {
+					sort: "-likes,-comments",
+				},
+			});
+			setPopularArticles({
+				popularArticles: response.data.result,
+				loading: false,
+				error: null,
+			});
+		} catch (err) {
+			setPopularArticles({
+				popularArticles: [],
+				loading: false,
+				error: err,
+			});
+		}
 	};
-	const [comment, setComment] = useState("");
-	const [comments, setComments] = useState([]),
-		onLikeButton = () => {
-			setLike(like + (isLike ? -1 : 1));
-			setIsLike(!isLike);
-		};
+
+	useEffect(() => {
+		fetchPopularArticles();
+		fetchArticles();
+	}, []);
+
 	return (
 		<div className='articles' style={{fontFamily: "Amaranth"}}>
 			<div className='container'>
@@ -59,146 +107,9 @@ export default function Articles() {
 								</div>
 							</div>
 							<div className='row gy-4'>
-								<div className='col-lg-6 col-sm-12'>
-									<div className='card border-0 box shadow rounded-3'>
-										<Link to={`/artdet`}>
-											<img src={child} className='card-img-top' alt='...' />
-											<div className='card-body content text-dark'>
-												<h5 className='card-title'>Are you tired of raising children?Here's what's easy for you </h5>
-												<p className='card-text'>
-													The modern mother is under great pressure to do everything! We tend to value the image of parents who do everything in their
-													power to provide a perfect life for their children
-												</p>
-											</div>
-										</Link>
-										<div className='info d-flex justify-content-between p-2'>
-											<p>Today</p>
-											<ul className='icons d-flex gap-4'>
-												<a>
-													<li>
-														<i className='bi bi-share' onClick={() => setShow(!show)}></i>
-													</li>
-												</a>
-												<a>
-													<li data-bs-toggle='modal' data-bs-target='#createModal'>
-														<i className='bi bi-chat-left'></i>
-													</li>
-												</a>
-												<a className='d-flex gap-1 text-dark'>
-													{like}
-													<li style={{color: "#FE6C93"}}>
-														<i className='bi bi-heart-fill' onClick={onLikeButton}></i>
-													</li>
-												</a>
-											</ul>
-										</div>
-									</div>
-									{/* comment */}
-									<div
-										className='modal fade'
-										id='createModal'
-										tabindex='-1'
-										aria-labelledby='createModalLabel'
-										aria-hidden='true'
-										data-bs-backdrop='false'
-									>
-										<div className='modal-dialog modal-dialog-centered'>
-											<div className='modal-content'>
-												<div className='modal-header align-items-center'>
-													<h5 className='text-dark text-center w-100 m-0' id='exampleModalLabel'>
-														Create Comment
-													</h5>
-													<button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
-												</div>
-												<div className='main-container'>
-													{comments.map((text) => (
-														<div className='bg-white rounded p-2 mt-2'>
-															<div className='d-flex justify-content-between'>
-																<div className='d-flex'>
-																	<img src={comme} alt='avatar' className='rounded-circle me-2' />
-																	<div className='comment-container p-1 rounded' style={{background: "#EEE"}}>
-																		<p className='m-0 fw-bold'>DR.Ahmed</p>
-																		{text}
-																	</div>
-																</div>
-															</div>
-														</div>
-													))}
-												</div>
-												<div className='modal-body'>
-													<div className='d-flex'>
-														<div className='p-1'>
-															<img src={comme} alt='avatar' className='rounded-circle' />
-														</div>
-														<div className='w-100'>
-															<input
-																type='text'
-																className='form-control rounded-pill bg-gray'
-																value={comment}
-																onChange={onChangeHandler}
-																placeholder='Write a comment...'
-															/>
-														</div>
-														<div className='p-1'>
-															<Link>
-																<div
-																	className='icon rounded-circle text-white text-center p-1'
-																	onClick={onClickHandler}
-																	style={{background: "#32aa90", width: "30px", height: "30px", transform: "rotate(45deg)"}}
-																>
-																	<i className='bi bi-send-fill me-1'></i>
-																</div>
-															</Link>
-														</div>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-									{/* end_comment */}
-									{/* share */}
-									{show && (
-										<div className='share'>
-											<div className='card border-0 shadow w-75 p-3 mt-3 ms-5'>
-												<div className='row'>
-													<div className='col'>
-														<FacebookShareButton url={"https://www.youtube.com/watch?v=2BnTYEafRQc"} quote={"Dummy text!"} hashtag='#muo'>
-															<FacebookIcon size={50} round />
-														</FacebookShareButton>
-													</div>
-													<div className='col'>
-														<TwitterShareButton url={"https://www.youtube.com/watch?v=2BnTYEafRQc"} quote={"Dummy text!"} hashtag='#muo'>
-															<TwitterIcon size={50} round />
-														</TwitterShareButton>
-													</div>
-													<div className='col'>
-														<WhatsappShareButton url={"https://www.youtube.com/watch?v=2BnTYEafRQc"} quote={"Dummy text!"} hashtag='#muo'>
-															<WhatsappIcon size={50} round />
-														</WhatsappShareButton>
-													</div>
-												</div>
-												<div className='row mt-3'>
-													<div className='col'>
-														<LinkedinShareButton url={"https://www.youtube.com/watch?v=2BnTYEafRQc"} quote={"Dummy text!"} hashtag='#muo'>
-															<LinkedinIcon size={50} round />
-														</LinkedinShareButton>
-													</div>
-													<div className='col'>
-														<TelegramShareButton url={"https://www.youtube.com/watch?v=2BnTYEafRQc"} quote={"Dummy text!"} hashtag='#muo'>
-															<TelegramIcon size={50} round />
-														</TelegramShareButton>
-													</div>
-													<div className='col'>
-														<EmailShareButton url={"https://www.youtube.com/watch?v=2BnTYEafRQc"} quote={"Dummy text!"} hashtag='#muo'>
-															<EmailIcon size={50} round />
-														</EmailShareButton>
-													</div>
-												</div>
-											</div>
-										</div>
-									)}
-									{/* end-share */}
-								</div>
+								{articles?.articles.map((article) => (
+									<Article article={article} />
+								))}
 							</div>
 						</div>
 						<div className='col-sm-4'>
@@ -235,60 +146,25 @@ export default function Articles() {
 							<div className='card mt-5 popular shadow rounded-3 border-0 h-25' style={{background: "rgba(221, 221, 221, 0.16)"}}>
 								<h4 className='text-center fw-bold'>Most Popular</h4>
 								<div className='baby_card mt-1'>
-									<div className='card mb-3 shadow border-top border-3 border-warning rounded-3 border-0 bg-white p-2' style={{borderTop: ""}}>
-										<div className='row'>
-											<div className='d-flex'>
-												<div className='col-md-4' style={{width: "40%"}}>
-													<img src={baby} alt='...' />
-												</div>
-												<div className='col-md-8'>
-													<div className='card-body'>
-														<h5 className='card-title text-primary'>
-															Causes of cold
-															<br /> extremities in <br />
-															infants
-														</h5>
+									{popularArticles.popularArticles.map((article) => (
+										<div className='card mb-3 shadow border-top border-3 border-warning rounded-3 border-0 bg-white p-2' style={{borderTop: ""}}>
+											<div className='row'>
+												<div className='d-flex'>
+													<div className='col-md-4' style={{width: "40%"}}>
+														<img src={article.articleCover.url} alt={article.title} />
 													</div>
+													<div className='col-md-8'>
+														<div className='card-body'>
+															<h5 className='card-title text-primary'>{article.title}</h5>
+														</div>
+													</div>
+													{/* style */}
+													{article.likes}
+													{article.comments}
 												</div>
 											</div>
 										</div>
-									</div>
-									<div className='card mb-3 shadow border-top border-3 border-warning rounded-3 border-0 bg-white p-2' style={{borderTop: ""}}>
-										<div className='row'>
-											<div className='d-flex'>
-												<div className='col-md-4' style={{width: "40%"}}>
-													<img src={baby} alt='...' />
-												</div>
-												<div className='col-md-8'>
-													<div className='card-body'>
-														<h5 className='card-title text-primary'>
-															Causes of cold
-															<br /> extremities in <br />
-															infants
-														</h5>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-									<div className='card mb-3 shadow border-top border-3 border-warning rounded-3 border-0 bg-white p-2' style={{borderTop: ""}}>
-										<div className='row'>
-											<div className='d-flex'>
-												<div className='col-md-4' style={{width: "40%"}}>
-													<img src={baby} alt='...' />
-												</div>
-												<div className='col-md-8'>
-													<div className='card-body'>
-														<h5 className='card-title text-primary'>
-															Causes of cold
-															<br /> extremities in <br />
-															infants
-														</h5>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
+									))}
 								</div>
 							</div>
 						</div>
@@ -297,4 +173,214 @@ export default function Articles() {
 			</div>
 		</div>
 	);
-}
+};
+
+const CommentComponent = ({articleComments, articleID}) => {
+	const navigate = useNavigate();
+	const [comment, setComment] = useState("");
+	const handleSendComment = async () => {
+		if (comment.trim() === "") {
+			return;
+		}
+		try {
+			const response = await axios.post(
+				baseURL + "article-comments",
+				{
+					comment: comment,
+					article: articleID,
+				},
+				{
+					headers: {
+						token: localStorage.getItem("token") ?? navigate("/login"),
+					},
+				},
+			);
+			setComment("");
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	return (
+		<div className='modal fade' id='createModal' tabIndex='-1' aria-labelledby='createModalLabel' aria-hidden='true' data-bs-backdrop='false'>
+			<div className='modal-dialog modal-dialog-centered'>
+				<div className='modal-content'>
+					<div className='modal-header align-items-center'>
+						<h5 className='text-dark text-center w-100 m-0' id='exampleModalLabel'>
+							Create Comment
+						</h5>
+						<button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+					</div>
+					<div className='main-container'>
+						{articleComments.map((comment) => (
+							<div className='bg-white rounded p-2 mt-2'>
+								<div className='d-flex justify-content-between'>
+									<div className='d-flex'>
+										<img
+											src={comment?.user?.profileImage?.url}
+											style={{width: "30px", height: "30px"}}
+											alt='avatar'
+											className='rounded-circle me-2'
+										/>
+										<div className='comment-container p-1 rounded' style={{background: "#EEE"}}>
+											<p className='m-0 fw-bold'>{comment?.user?.username}</p>
+											<p className='m-0'>{comment?.comment}</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
+					<div className='modal-body'>
+						<div className='d-flex'>
+							<div className='p-1'>
+								<img src={comme} alt='avatar' className='rounded-circle' />
+							</div>
+							<div className='w-100'>
+								<input
+									type='text'
+									className='form-control rounded-pill bg-gray'
+									value={comment}
+									onChange={(e) => setComment(e.target.value)}
+									placeholder='Write a comment...'
+								/>
+							</div>
+							<div onClick={handleSendComment} style={{cursor: "pointer"}} className='p-1'>
+								<div
+									className='icon rounded-circle text-white text-center p-1'
+									style={{background: "#32aa90", width: "30px", height: "30px", transform: "rotate(45deg)"}}
+								>
+									<i className='bi bi-send-fill me-1'></i>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const Article = ({article}) => {
+	const doesUserLikeThis = article.usersLiked
+		.map((user) => {
+			return user._id;
+		})
+		.includes(localStorage.getItem("ID"));
+
+	const navigate = useNavigate();
+	const [share, setShare] = useState(false);
+	const [like, setLike] = useState(doesUserLikeThis);
+
+	return (
+		<>
+			<div key={article._id} className='col-lg-6 col-sm-12'>
+				{
+					<div className='card border-0 box shadow rounded-3'>
+						<Link to={`/artdet/${article?._id}`}>
+							<img src={child} className='card-img-top' alt='...' />
+							<div className='card-body content text-dark'>
+								<h5 className='card-title'>{article?.title} </h5>
+								<p className='card-text'>{article?.description.slice(0, 200)}...</p>
+							</div>
+						</Link>
+						<div className='info d-flex justify-content-between p-2'>
+							<p>
+								{isToday(parseISO(article?.updatedAt))
+									? "Today"
+									: isYesterday(parseISO(article?.updatedAt))
+									? "Yesterday"
+									: format(parseISO(article?.updatedAt), "yyyy-MM-dd")}
+							</p>
+							<ul className='icons d-flex gap-4'>
+								<a>
+									<li>
+										<i
+											className='bi bi-share'
+											onClick={() => {
+												setShare(!share);
+											}}
+										></i>
+									</li>
+								</a>
+								<a>
+									<li data-bs-toggle='modal' data-bs-target='#createModal'>
+										<i className='bi bi-chat-left'></i>
+									</li>
+								</a>
+								<a className='d-flex gap-1 text-dark'>
+									{article?.likes + (like ? 1 : 0)}
+									<li style={{color: "#FE6C93"}}>
+										<i
+											className={`bi bi-heart${like ? "-fill" : ""}`}
+											onClick={async () => {
+												setLike(!like);
+												try {
+													const method = like ? "DELETE" : "PATCH";
+													const url = `${baseURL}articles/likes/${article._id}`;
+													const response = await axios({
+														method: method,
+														url: url,
+														headers: {
+															token: localStorage.getItem("token") ?? navigate("/login"),
+														},
+													});
+													console.log(response);
+												} catch (err) {
+													console.error(err);
+												}
+											}}
+										></i>
+									</li>
+								</a>
+							</ul>
+						</div>
+					</div>
+				}
+				<CommentComponent articleComments={article.articleComments} articleID={article._id} />
+				{share && (
+					<div className='share'>
+						<div className='card border-0 shadow w-75 p-3 mt-3 ms-5'>
+							<div className='row'>
+								<div className='col'>
+									<FacebookShareButton url={"https://www.youtube.com/watch?v=2BnTYEafRQc"} quote={"Dummy text!"} hashtag='#muo'>
+										<FacebookIcon size={50} round />
+									</FacebookShareButton>
+								</div>
+								<div className='col'>
+									<TwitterShareButton url={"https://www.youtube.com/watch?v=2BnTYEafRQc"} quote={"Dummy text!"} hashtag='#muo'>
+										<TwitterIcon size={50} round />
+									</TwitterShareButton>
+								</div>
+								<div className='col'>
+									<WhatsappShareButton url={"https://www.youtube.com/watch?v=2BnTYEafRQc"} quote={"Dummy text!"} hashtag='#muo'>
+										<WhatsappIcon size={50} round />
+									</WhatsappShareButton>
+								</div>
+							</div>
+							<div className='row mt-3'>
+								<div className='col'>
+									<LinkedinShareButton url={"https://www.youtube.com/watch?v=2BnTYEafRQc"} quote={"Dummy text!"} hashtag='#muo'>
+										<LinkedinIcon size={50} round />
+									</LinkedinShareButton>
+								</div>
+								<div className='col'>
+									<TelegramShareButton url={"https://www.youtube.com/watch?v=2BnTYEafRQc"} quote={"Dummy text!"} hashtag='#muo'>
+										<TelegramIcon size={50} round />
+									</TelegramShareButton>
+								</div>
+								<div className='col'>
+									<EmailShareButton url={"https://www.youtube.com/watch?v=2BnTYEafRQc"} quote={"Dummy text!"} hashtag='#muo'>
+										<EmailIcon size={50} round />
+									</EmailShareButton>
+								</div>
+							</div>
+						</div>
+					</div>
+				)}
+			</div>
+		</>
+	);
+};
+
+export default Articles;
