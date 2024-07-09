7e28d47e8aa046fd1f6c9f06055748955fed4593
Saeedinum <mo7amed0ptr@gmail.com>
2024-07-09 09:35:11 +0300

    add skeleton

--- src/features/ecommerce/favourites/Skeleton.jsx
+++ src/features/ecommerce/favourites/Skeleton.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import ContentLoader from "react-content-loader";
 
-const Skeleton = () => {
-  return (
-    <div>Skeleton</div>
-  )
-}
+const Skeleton = () => (
+	<ContentLoader speed={2} width={400} height={400} viewBox='0 0 400 400' backgroundColor='#d6d6d6' foregroundColor='#ffffff'>
+		<rect x='8' y='259' rx='2' ry='2' width='226' height='18' />
+		<circle cx='122' cy='119' r='114' />
+		<rect x='147' y='288' rx='2' ry='2' width='60' height='10' />
+		<rect x='64' y='312' rx='8' ry='8' width='100' height='25' />
+	</ContentLoader>
+);
 
-export default Skeleton
+export default Skeleton;
