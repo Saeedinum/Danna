0569de89f337b58513bd07e5274e78c9fc0088f2
Saeedinum <mo7amed0ptr@gmail.com>
2024-07-13 15:03:33 +0300

    move Router Provider in a different directory

--- src/index.jsx
+++ src/index.jsx
@@ -6,140 +6,14 @@ import "react-calendar/dist/Calendar.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import {RouterProvider, createBrowserRouter} from "react-router-dom";
+
 import {ToastContainer} from "react-toastify";
 import {Offline} from "react-detect-offline";
 
-import Home from "./pages/home/Home.jsx";
-import Login from "./pages/login/Login.jsx";
-import Register from "./pages/signup/Signup.jsx";
-import AboutUs from "./pages/AboutUs";
-import ContactUs from "./pages/ContactUs";
-import ChatBot from "./pages/ChatBot";
-import Notfound from "./pages/error/Notfound.jsx";
-import Payment from "./pages/Payment.jsx";
-import Questions from "./pages/questions/Questions.jsx";
-import QuestionDet from "./pages/questions/QuestionDetails.jsx";
-
-import Articles from "./features/articles/Articles.jsx";
-import ShowArticle from "./features/articles/ShowArticle.jsx";
-
-import Doctors from "./features/doctors/Doctors.jsx";
-import DetailsDoctors from "./features/doctors/DetailsDoctor.jsx";
-
-import ShowProduct from "./features/ecommerce/products/ShowProduct.jsx";
-import ProductsPage from "./features/ecommerce/products/ProductsPage.jsx";
-import Cart from "./features/ecommerce/cart/Cart.jsx";
-import Favourite from "./features/ecommerce/favourites/Favourite.jsx";
-import PaymentProduct from "./features/ecommerce/cart/paymentProduct.jsx";
-
-import ProtectedRoutes from "./features/authentication/ProtectedRoutes.jsx";
-import UpdateProfile from "./features/authentication/UpdateProfile.jsx";
-import ForgetPassword from "./features/authentication/forget/ForgetPassword.jsx";
-
-import ProfileDoctor from "./features/doctors/ProfileDr.jsx";
-import Videos from "./features/videos/Videos.jsx";
-
-import Book from "./features/booking/Book.jsx";
-import BookDet from "./features/booking/BookDetail.jsx";
-
-import Celendar1 from "./components/calendar/Celendar.jsx";
-
-import MainLayout from "./layouts/MainLayout.jsx";
-import AuthLayout from "./layouts/AuthLayout.jsx";
+import Router from "./router/Router.jsx";
 
 import {Provider} from "react-redux";
 import store from "./store/store.js";
-
-const routes = createBrowserRouter([
-	{
-		path: "/",
-		element: <MainLayout />,
-		children: [
-			{index: true, element: <Home />},
-			{path: "signup", element: <Register />},
-			{path: "login", element: <Login />},
-			{path: "profile", element: <UpdateProfile />},
-			{path: "home", element: <Home />},
-			{path: "articles", element: <Articles />},
-			{path: "articleDetails/:id", element: <ShowArticle />},
-			{path: "doctors", element: <Doctors />},
-			{path: "celendar1", element: <Celendar1 />},
-			{path: "aboutus", element: <AboutUs />},
-			{path: "detailsdoctors/:doctorID", element: <DetailsDoctors />},
-			{path: "productpage/:id?", element: <ProductsPage />},
-			{path: "profiledr", element: <ProfileDoctor />},
-			{path: "contactus", element: <ContactUs />},
-			{
-				path: "chatbot",
-				element: (
-					<ProtectedRoutes>
-						<ChatBot />
-					</ProtectedRoutes>
-				),
-			},
-			{path: "productDesply/:productID", element: <ShowProduct />},
-			{
-				path: "Cart",
-				element: (
-					<ProtectedRoutes>
-						<Cart />
-					</ProtectedRoutes>
-				),
-			},
-			{path: "payment", element: <Payment />},
-			{path: "videos/:id?", element: <Videos />},
-			{
-				path: "favourite",
-				element: (
-					<ProtectedRoutes>
-						<Favourite />
-					</ProtectedRoutes>
-				),
-			},
-			{
-				path: "Questions",
-				element: <Questions />,
-			},
-			{
-				path: "QuestionDet",
-				element: <QuestionDet />,
-			},
-
-			{
-				path: "ForgetPassword",
-				element: <ForgetPassword />,
-			},
-
-			{
-				path: "Book",
-				element: <Book />,
-			},
-
-			{
-				path: "Bookdetail",
-				element: <BookDet />,
-			},
-			{
-				path: "PayProduct",
-				element: (
-					<ProtectedRoutes>
-						<PaymentProduct />
-					</ProtectedRoutes>
-				),
-			},
-			{path: "*", element: <Notfound />},
-		],
-	},
-	{
-		path: "/",
-		element: <AuthLayout />,
-		children: [
-			{path: "signup", element: <Register />},
-			{path: "login", element: <Login />},
-		],
-	},
-]);
 
 // if (process.env.NODE_ENV === "production") {
 // 	if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ === "object") {
@@ -154,7 +28,7 @@ root.render(
 	// <React.StrictMode>
 	<div className='App'>
 		<Provider store={store}>
-			<RouterProvider router={routes} />
+			<Router />
 		</Provider>
 		<Offline>
 			<div className='offline'>You are Offline Now!</div>
